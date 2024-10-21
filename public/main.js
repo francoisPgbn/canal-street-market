3d351abfd55c97f6a3492e9f9104c692ac6400a7
Francois Panganiban <francois.panganiban@estiam.com>
2024-10-21 12:42:40 +0200

    compress

---
+++ public/main.js
@@ -0,0 +1,74 @@
+const main = document.querySelector(".main");
+const btnBlue = document.querySelector(".btn-nav__blue");
+const btnRed = document.querySelector(".btn-nav__red");
+const btnYellow = document.querySelector(".btn-nav__yellow");
+const btnLogo = document.querySelector(".btn-logo");
+const food = document.querySelector(".food");
+const retail = document.querySelector(".retail");
+const community = document.querySelector(".community");
+ 
+btnBlue.addEventListener("click", () => {
+  const isOpened = food.getAttribute("aria-expanded");
+  if (isOpened === "false") {
+    food.setAttribute("aria-expanded", "true");
+    main.setAttribute("aria-expanded", "false");
+    main.setAttribute("initial-state", "false");
+    retail.setAttribute("aria-expanded", "false");
+    community.setAttribute("aria-expanded", "false");
+    btnLogo.setAttribute("aria-expanded", "false");
+  } else {
+    main.setAttribute("aria-expanded", "true");
+    btnLogo.setAttribute("aria-expanded", "true");
+    food.setAttribute("aria-expanded", "false");
+    retail.setAttribute("aria-expanded", "false");
+    community.setAttribute("aria-expanded", "false");
+  }
+});
+
+btnRed.addEventListener("click", () => {
+  const isOpened = retail.getAttribute("aria-expanded");
+  if (isOpened === "false") {
+    retail.setAttribute("aria-expanded", "true");
+    main.setAttribute("aria-expanded", "false");
+    main.setAttribute("initial-state", "false");
+    food.setAttribute("aria-expanded", "false");
+    community.setAttribute("aria-expanded", "false");
+    btnLogo.setAttribute("aria-expanded", "false");
+  } else {
+    main.setAttribute("aria-expanded", "true");
+    btnLogo.setAttribute("aria-expanded", "true");
+    food.setAttribute("aria-expanded", "false");
+    retail.setAttribute("aria-expanded", "false");
+    community.setAttribute("aria-expanded", "false");
+  }
+});
+
+btnYellow.addEventListener("click", () => {
+  const isOpened = community.getAttribute("aria-expanded");
+  if (isOpened === "false") {
+    community.setAttribute("aria-expanded", "true");
+    main.setAttribute("aria-expanded", "false");
+    main.setAttribute("initial-state", "false");
+    retail.setAttribute("aria-expanded", "false");
+    food.setAttribute("aria-expanded", "false");
+    btnLogo.setAttribute("aria-expanded", "false");
+  } else {
+    btnLogo.setAttribute("aria-expanded", "true");
+    main.setAttribute("aria-expanded", "true");
+    food.setAttribute("aria-expanded", "false");
+    retail.setAttribute("aria-expanded", "false");
+    community.setAttribute("aria-expanded", "false");
+  }
+});
+
+btnLogo.addEventListener("click", () => {
+  const isOpened = btnLogo.getAttribute("aria-expanded");
+  if (isOpened === "false") {
+    btnLogo.setAttribute("aria-expanded", "true");
+    main.setAttribute("aria-expanded", "true");
+    community.setAttribute("aria-expanded", "false");
+    main.setAttribute("initial-state", "false");
+    retail.setAttribute("aria-expanded", "false");
+    food.setAttribute("aria-expanded", "false");
+  }
+});
